fd6a6eccf7e469718189198010fbb55f03287e7b
Jye Lee <jlee@cloudpassage.com>
2017-03-13 11:43:55 -0700

    make expires_in mandatory only for execute

--- kernel_exceptions.py
+++ kernel_exceptions.py
@@ -19,6 +19,9 @@ class KernelExceptions(object):
         if not self.options['report'] and not self.options['execute']:
             print 'Please specify --report or --execute'
             exit(0)
+        if self.options['execute'] and not self.options['expires_in']:
+            print 'Please specify --expires_in=<number of days> if using --execute'
+            exit(0)
 
     def main(self):
         self.guard()
@@ -27,12 +30,12 @@ class KernelExceptions(object):
             kernels = self.sc.extra_kernels(srv['id'])
             if kernels:
                 total.append(kernels)
-            if kernels and self.options['execute']:
-                self.report(srv, kernels)
-                self.ec.add_exceptions(srv, kernels)
-                print 'Exceptions for non-running vulnerable kernel packages are added.'
-            else:
-                self.report(srv, kernels)
+                if self.options['execute'] and kernels:
+                    self.report(srv, kernels)
+                    self.ec.add_exceptions(srv, kernels)
+                    print 'Exceptions for non-running vulnerable kernel packages are added.'
+                else:
+                    self.report(srv, kernels)
         if not total:
             print 'No servers with more than one Kernel package has been found'
 
